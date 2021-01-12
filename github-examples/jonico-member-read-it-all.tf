5c4105569d96d50309cdd7db0a2f4f1a6a2e80ad
Johannes Nicolai <jonico@github.com>
2021-01-12 17:31:18 +0100

    Update jonico-member-read-it-all.tf

--- github-examples/jonico-member-read-it-all.tf
+++ github-examples/jonico-member-read-it-all.tf
@@ -1,5 +1,5 @@
 resource "github_team_membership" "jonico-member-read-it-all" {
-  team_id  = github_team.read-it-all.id"
+  team_id  = github_team.read-it-all.id
   username = "jonico"
   role     = "member"
 }
