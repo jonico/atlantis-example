5b9776b1fafc77151da725ab095d7639d040a6d3
Johannes Nicolai <jonico@github.com>
2021-01-12 17:02:05 +0100

    GitHub Terraform Provider Examples

---
+++ github-examples/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.12, < 0.15"
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-jonico"
+    key = "github-examples/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks-jonico"
+    encrypt = true
+  }
+}
+
+provider "github" {
+  token = "${var.github_token}"
+  owner = "${var.github_owner}"
+}
