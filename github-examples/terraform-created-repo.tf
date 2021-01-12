17ef6a821c493dbd1acaedc6e42e831526ada18e
Johannes Nicolai <jonico@github.com>
2021-01-12 18:39:42 +0100

    Update terraform-created-repo.tf

--- github-examples/terraform-created-repo.tf
+++ github-examples/terraform-created-repo.tf
@@ -1,6 +1,6 @@
 resource "github_repository" "terraform-created" {
-  name        = "terraform-created"
-  description = "Repo created by terraform"
+  name        = "terraform-examples"
+  description = "Repo created by terraform as an example on how to use the GitHub Terraform provider"
 
   visibility = "private"
 
