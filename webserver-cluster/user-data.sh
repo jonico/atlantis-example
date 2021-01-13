f92a8bee0264fa30d6ee1fe0604e79dab3befa90
Johannes Nicolai <jonico@github.com>
2021-01-13 12:27:19 +0100

    Binding Jekyll to all network adapters

--- webserver-cluster/user-data.sh
+++ webserver-cluster/user-data.sh
@@ -9,4 +9,4 @@ EOF
 su - bitnami
 export PATH='/opt/bitnami/rvm/bin:/opt/bitnami/memcached/bin:/opt/bitnami/perl/bin:/opt/bitnami/git/bin:/opt/bitnami/nginx/sbin:/opt/bitnami/redis/bin:/opt/bitnami/nodejs/bin:/opt/bitnami/sphinx/bin:/opt/bitnami/sqlite/bin:/opt/bitnami/varnish/bin:/opt/bitnami/php/bin:/opt/bitnami/python/bin:/opt/bitnami/mysql/bin:/opt/bitnami/postgresql/bin:/opt/bitnami/letsencrypt/:/opt/bitnami/apache2/bin:/opt/bitnami/subversion/bin:/opt/bitnami/ruby/bin:/opt/bitnami/common/bin:/opt/bitnami/memcached/bin:/opt/bitnami/perl/bin:/opt/bitnami/git/bin:/opt/bitnami/nginx/sbin:/opt/bitnami/redis/bin:/opt/bitnami/nodejs/bin:/opt/bitnami/sphinx/bin:/opt/bitnami/sqlite/bin:/opt/bitnami/varnish/bin:/opt/bitnami/php/bin:/opt/bitnami/python/bin:/opt/bitnami/mysql/bin:/opt/bitnami/postgresql/bin:/opt/bitnami/letsencrypt/:/opt/bitnami/apache2/bin:/opt/bitnami/subversion/bin:/opt/bitnami/ruby/bin:/opt/bitnami/common/bin:/usr/local/bin:/usr/bin:/bin:/usr/games'
 cd /opt/bitnami/projects/jekyll
-nohup bundle exec jekyll serve &
+nohup bundle exec jekyll serve --host=0.0.0.0 &
