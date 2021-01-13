f64dc04aff3e880adfa884589dcca4624d45c59c
Johannes Nicolai <jonico@github.com>
2021-01-13 13:05:36 +0100

    New Jekyll image

--- webserver-cluster/main.tf
+++ webserver-cluster/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id        = "ami-02edafac88041ca1d"
+  image_id        = "ami-0f14407b76a3dc349"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
   user_data       = data.template_file.user_data.rendered
